4bada1d0faa999d831b77efebd2ef9b71fedb9ad
Shirisha868 <kolupulashirisha08@gmail.com>
2024-11-22 12:20:01 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,13 @@
+ 
+print("Multiplication generator")
+while True:
+    n=int(input("enter a number:"))
+    if(n==0):
+        print("Thank you!!")
+        break
+    else:
+        for i in range(1,11):
+            print(n,"*",i,"=",n*i) 
+      
+
+ 
